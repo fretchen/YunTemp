4d1a6b5c935b51ad7ac570764b2b31883079a3ab
Fred J <fred.jendrzejewski@gmail.com>
2020-01-14 07:29:41 +0100

    Simplify the yuntemp

--- yuntemp.py
+++ yuntemp.py
@@ -3,7 +3,7 @@ An extremely simple server that might be used for testing the arduino webserver 
 '''
 
 from flask import Flask, request, jsonify
-import numpy as np
+import random
 app = Flask(__name__)
 
 setpoint = 0;
@@ -15,7 +15,7 @@ tauD = 0;
 
 @app.route('/arduino/read/all/')
 def get_temp():
-    meas = np.random.randint(700, 800);
+    meas = random.randint(700, 800);
     global setpoint, gain, tauI, err, control, tauD;
     print(setpoint)
     first_line = "setpoint, input, error, output, G, tauI, tauD <br />";
@@ -29,7 +29,7 @@ def set_setpoint(n_val):
     global setpoint, gain, tauI, err, control, tauD;
     setpoint = n_val;
     print(setpoint);
-    meas = np.random.randint(700, 800);
+    meas = random.randint(700, 800);
     first_line = "setpoint, input, error, output, G, tauI, tauD <br />";
     ard_str = str(setpoint) + ',' + str(meas) + ',' + str(err) + ',' + str(control)
     ard_str = ard_str + ',' + str(gain) + ',' + str(tauI) +',' + str(tauD);
@@ -41,7 +41,7 @@ def set_integral(n_val):
     global setpoint, gain, tauI, err, control, tauD;
     tauI = n_val;
     print(tauI);
-    meas = np.random.randint(700, 800);
+    meas = random.randint(700, 800);
     first_line = "setpoint, input, error, output, G, tauI, tauD <br />";
     ard_str = str(setpoint) + ',' + str(meas) + ',' + str(err) + ',' + str(control)
     ard_str = ard_str + ',' + str(gain) + ',' + str(tauI) +',' + str(tauD);
@@ -53,7 +53,7 @@ def set_differential(n_val):
     global setpoint, gain, tauI, err, control, tauD;
     tauD = n_val;
     print(tauD);
-    meas = np.random.randint(700, 800);
+    meas = random.randint(700, 800);
     first_line = "setpoint, input, error, output, G, tauI, tauD <br />";
     ard_str = str(setpoint) + ',' + str(meas) + ',' + str(err) + ',' + str(control)
     ard_str = ard_str + ',' + str(gain) + ',' + str(tauI) +',' + str(tauD);
@@ -65,7 +65,7 @@ def set_gain(n_val):
     global setpoint, gain, tauI, err, control, tauD;
     gain = n_val;
     print(gain);
-    meas = np.random.randint(700, 800);
+    meas = random.randint(700, 800);
     first_line = "setpoint, input, error, output, G, tauI, tauD <br />";
     ard_str = str(setpoint) + ',' + str(meas) + ',' + str(err) + ',' + str(control)
     ard_str = ard_str + ',' + str(gain) + ',' + str(tauI) +',' + str(tauD);
